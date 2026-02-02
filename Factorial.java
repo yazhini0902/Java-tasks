e51e13fc40b0b8105714a3082d2c2e37ca0c327c
yazhini0902 <yazhini0923@gmail.com>
2026-02-02 18:58:05 +0530

    Add Factorial program in Java

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package javanew;
+import java.util.Scanner;
+public class factorial
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Enter a number:");
+		int a=sc.nextInt();
+		int fact=1;
+		int i=1;
+		while(i<=a)
+		{
+			fact=fact*i;
+			i++;
+		}
+		System.out.println(fact);
+}
+}
